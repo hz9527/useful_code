5d0142cb915195a53e88269ff9d9c65bbb4faca8
huangzhong <huangzhong@daojia.com>
2017-11-21 19:49:27 +0800

    test sh

--- build/updateWebsite.sh
+++ build/updateWebsite.sh
@@ -1,2 +1,8 @@
-git checkout -b gh-pages
-echo $?
+read -p "are you sure publish webSite ? do not remember commit code  " -n 1 choose
+if [[ $choose =~ ^[Yy]$ ]];then
+  echo -n 'checkout gh-pages'
+  git checkout gh-pages
+  if [[ 0 == $? ]];then
+    echo -n 'checkout success'
+  fi
+fi
